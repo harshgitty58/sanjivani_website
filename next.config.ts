eca768563894006774ca38fd42a4aaa4c9f43125
sanjivaningo <rahultorambe@sanjivani.org>
2025-10-19 12:11:45 +0530

    x

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import path from 'path'
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   /* merged static assets under public/ngo-banner-next */
+   outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
